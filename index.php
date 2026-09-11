abc2709953ffde3deb3798f157812ac28fb9cca7
Volodymyr Kamuz <v.kamuz@gmail.com>
2026-09-11 19:04:59 +0300

    Add button and style

--- index.php
+++ index.php
@@ -10,10 +10,12 @@ get_header(); ?>
 		<h1 class="title"><?php bloginfo( 'name' ); ?></h1>
 		<?php if ( have_posts() ) : ?>
 			<ul id="posts-list">
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php
+				while ( have_posts() ) : the_post(); ?>
 					<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 				<?php endwhile; ?>
 			</ul>
+			<?php get_template_part( 'partials/loadmore' ); ?>
 			<div class="pagination">
 				<?php
 				global $wp_query;
